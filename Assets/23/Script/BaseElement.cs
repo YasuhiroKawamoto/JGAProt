16ece36c9d56308499992756a8ecda6f79db4336
slovenlyboy <slovenlyboy@gmail.com>
2018-04-05 14:36:03 +0900

    エネルギー上限、生成エレメント、バグの修正

---
+++ Assets/23/Script/BaseElement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Play.Element
+{
+
+
+    public class BaseElement : MonoBehaviour
+    {
+
+        [SerializeField]
+        private int _chageSpeed;//エネルギーチャージ速度
+
+        [SerializeField]
+        private bool _isChage;//チャージ状態かどうか？
+
+
+
+        // Use this for initialization
+        void Start()
+        {
+            _chageSpeed = 10;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            //チャージ状態なら
+            if (_isChage)
+            {
+                //時間経過とともに回復
+                gameObject.GetComponent<Element>().ChageEnergy(_chageSpeed * Time.deltaTime);
+            }
+
+            if (gameObject.GetComponent<Element>().GetState() != 0)
+            {
+                _isChage = false;
+            }
+            else
+            {
+                _isChage = true;
+            }
+
+        }
+    }
+
+}
---
+++ Assets/23/Script/EnergyImage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Play.Element
+{
+    public class EnergyImage : MonoBehaviour
+    {
+
+        SpriteRenderer _spriteRenderer;
+
+        // Use this for initialization
+        void Start()
+        {
+            _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            double size =  gameObject.GetComponentInParent<Element>().GetEnergy()/gameObject.GetComponentInParent<Element>().GetCapacity();
+
+            _spriteRenderer.GetComponent<SpriteRenderer>().transform.localScale = new Vector3((float)size,(float)size,2);
+        }
+    }
+}
