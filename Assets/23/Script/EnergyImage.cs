c7d7db59eabab8e14e45b669960f64ff6dd29e09
slovenlyboy <slovenlyboy@gmail.com>
2018-04-06 13:16:38 +0900

    enum設定、バグ修正、生成側に送れないように制限追加

--- Assets/23/Script/EnergyImage.cs
+++ Assets/23/Script/EnergyImage.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+/// <summary>
+/// エネルギー画像の動的サイズ変更
+/// </summary>
 namespace Play.Element
 {
     public class EnergyImage : MonoBehaviour
@@ -19,9 +21,10 @@ namespace Play.Element
         // Update is called once per frame
         void Update()
         {
-
+            //容量に対してのチャージ量で画像のサイズを算出
             double size =  gameObject.GetComponentInParent<Element>().GetEnergy()/gameObject.GetComponentInParent<Element>().GetCapacity();
 
+            //サイズを画像に反映
             if (size > 0)
             {
                 _spriteRenderer.GetComponent<SpriteRenderer>().transform.localScale = new Vector3((float)size, (float)size, 1);
