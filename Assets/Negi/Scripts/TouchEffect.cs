8b0c5d27683881deb2b1021c9dd442c4ff1d6505
DESKTOP-R1GNS6C\yamak <dqx_2527@yahoo.co.jp>
2018-04-07 14:52:42 +0900

    軽くエフェクトの出るタッチエフェクトの追加

---
+++ Assets/Negi/Scripts/TouchEffect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchEffect : MonoBehaviour {
+
+    // タップエフェクト
+    [SerializeField]
+    ParticleSystem _tapEffect;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var camera = Camera.main;
+            // マウスのワールド座標までパーティクルを移動し、パーティクルエフェクトを1つ生成する
+            var pos = camera.ScreenToWorldPoint(Input.mousePosition + camera.transform.forward * 10);
+            GameObject obj = GameObject.Instantiate(_tapEffect.gameObject, pos, Quaternion.identity);
+            var particle = obj.GetComponent<ParticleSystem>();
+            particle.Emit(1);
+            Destroy(obj, particle.main.duration);
+        }
+    }
+}
