9573dbe858e1271d03115ca783e88612bde6e20b
DESKTOP-R1GNS6C\yamak <dqx_2527@yahoo.co.jp>
2018-04-08 12:16:21 +0900

    PlaySceneの構成整え

---
+++ Assets/Negi/Scripts/PlayManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayManager : SingletonMonoBehaviour<PlayManager> {
+
+    // UIRoot
+    [SerializeField]
+    private GameObject _uiRoot = null;
+    public GameObject UIRoot
+    {
+        get { return _uiRoot; }
+        private set { _uiRoot = value; }
+    }
+
+    // 2DObjectのRoot
+    [SerializeField]
+    private GameObject _2dObjectRoot = null;
+    public GameObject ObjectRoot
+    {
+        get { return _2dObjectRoot; }
+        private set { _2dObjectRoot = value; }
+    }
+
+}
