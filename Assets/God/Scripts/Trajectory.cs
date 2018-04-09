3146dedb5534ace6cbc934a5e28eef9625a03bb5
TSITCL\s162054 <yasu.suika@gmail.com>
2018-04-09 12:07:10 +0900

    prefab自体をDestroyしないように修正

--- Assets/God/Scripts/Trajectory.cs
+++ Assets/God/Scripts/Trajectory.cs
@@ -19,6 +19,10 @@ namespace Play.Trajectory
         [SerializeField]
         private GameObject _trajectoryRoot;
 
+        // ルートオブジェクト(変数)
+        private GameObject _trajectoryRootValue;
+
+
         // タップ開始エレメント
         private Element.Element _element;
 
@@ -58,7 +62,7 @@ namespace Play.Trajectory
             //_effects = new List<Attack>();
 
             // 起動時に軌跡のルートを作成
-            _trajectoryRoot = Instantiate(_trajectoryRoot);
+            _trajectoryRootValue = Instantiate(_trajectoryRoot);
         }
 
         // Update is called once per frame
@@ -93,7 +97,7 @@ namespace Play.Trajectory
             {
                 _plot.transform.position = worldPos;
 
-                _lastObj = Instantiate(_plot, _trajectoryRoot.transform);
+                _lastObj = Instantiate(_plot, _trajectoryRootValue.transform);
                 _trajectory.Add(_lastObj);
             }
         }
@@ -124,7 +128,7 @@ namespace Play.Trajectory
 
             Vector3 pos = part.transform.position;
 
-            Attack atkObj = Instantiate(_attack, pos, Quaternion.identity, _trajectoryRoot.transform);
+            Attack atkObj = Instantiate(_attack, pos, Quaternion.identity, _trajectoryRootValue.transform);
             atkObj.Parent = _element;
             atkObj.Index = index;
             //_effects.Add( atkObj);
@@ -194,7 +198,7 @@ namespace Play.Trajectory
 
         public void DeleteTrajectoyRoot()
         {
-            Destroy(_trajectoryRoot);
+            Destroy(_trajectoryRootValue);
         }
     }
 }
